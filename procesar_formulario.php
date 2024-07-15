688cf3e41087f9b21f57d7513b8c48a7a1f96bed
TheSculper <49732446+TheSculper@users.noreply.github.com>
2024-07-15 02:20:17 -0400

    Add files via upload

--- procesar_formulario.php
+++ procesar_formulario.php
@@ -1,15 +1,28 @@
 <?php
-// Verificar reCAPTCHA
 $secretKey = "6LcYMhAqAAAAANYrsEiwgUDYaPhJnk57qsq1TEVk";
-$responseKey = $_POST['g-recaptcha-response'];
-$ip = $_SERVER['REMOTE_ADDR'];
+$token = $_POST['g-recaptcha-response'];
 
-$url = "https://www.google.com/recaptcha/api/siteverify?secret=$secretKey&response=$responseKey&remoteip=$ip";
-$response = file_get_contents($url);
+// Realizar la verificación del token usando cURL o cualquier método adecuado
+$url = "https://www.google.com/recaptcha/api/siteverify";
+$data = [
+    'secret' => $secretKey,
+    'response' => $token
+];
+
+$options = [
+    'http' => [
+        'header' => "Content-type: application/x-www-form-urlencoded\r\n",
+        'method' => 'POST',
+        'content' => http_build_query($data)
+    ]
+];
+
+$context = stream_context_create($options);
+$response = file_get_contents($url, false, $context);
 $responseKeys = json_decode($response, true);
 
 if ($responseKeys["success"]) {
-    // Procesar el formulario, ya que el usuario pasó la validación reCAPTCHA
+    // Procesar el formulario, ya que el usuario pasó la validación reCAPTCHA v3
     // ...
 } else {
     // Mostrar un mensaje de error o redirigir si el usuario no pasó la validación
