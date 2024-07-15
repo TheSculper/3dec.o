3e9f3c868a32f37e53c9973362df0b2796a96f33
TheSculper <49732446+TheSculper@users.noreply.github.com>
2024-07-15 00:15:35 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -4,13 +4,13 @@ document.getElementById('calculoForm').addEventListener('submit', function(event
     const materialCost = parseFloat(document.getElementById('material').value);
     const weight = parseFloat(document.getElementById('peso').value);
     const time = parseFloat(document.getElementById('tiempo').value);
-    const costPerHour = parseFloat(document.getElementById('costoHora').value);
+    const fixedCostPerHour = 5000; //Costo fijo por hora de impresión
 
-    if (isNaN(materialCost) || isNaN(weight) || isNaN(time) || isNaN(costPerHour)) {
+    if (isNaN(materialCost) || isNaN(weight) || isNaN(time) {
         alert('Por favor, ingrese valores válidos en todos los campos.');
         return;
     }
 
-    const totalCost = (materialCost * weight) + (costPerHour * time);
+    const totalCost = (materialCost * weight) + (fixedCostPerHour * time);
     document.getElementById('costoTotal').textContent = totalCost.toFixed(2);
 });
