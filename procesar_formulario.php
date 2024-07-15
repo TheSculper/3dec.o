83d54567e462348495b97b7083dc8af3865a2887
TheSculper <49732446+TheSculper@users.noreply.github.com>
2024-07-15 16:31:36 -0400

    Add files via upload

--- procesar_formulario.php
+++ procesar_formulario.php
@@ -1,21 +1,23 @@
 <?php
 $secretKey = "6LcYMhAqAAAAANYrsEiwgUDYaPhJnk57qsq1TEVk";
 $token = $_POST['g-recaptcha-response'];
-
-// Realizar la verificación del token usando cURL o cualquier método adecuado
 $url = "https://www.google.com/recaptcha/api/siteverify";
-$data = [
-    'secret' => $secretKey,
-    'response' => $token
-];
 
-$options = [
-    'http' => [
+$data = array(
+    'secret' => $secretKey,
+    'response' => $token, //algunos le ponen captcha
+    'remoteip' => $ip
+);
+
+$options = array(
+    'http' => array(
         'header' => "Content-type: application/x-www-form-urlencoded\r\n",
         'method' => 'POST',
         'content' => http_build_query($data)
-    ]
-];
+    )
+);
+
+//data y options estan en formato array()
 
 $context = stream_context_create($options);
 $response = file_get_contents($url, false, $context);
@@ -23,9 +25,28 @@ $responseKeys = json_decode($response, true);
 
 if ($responseKeys["success"]) {
     // Procesar el formulario, ya que el usuario pasó la validación reCAPTCHA v3
-    // ...
+    $to = "impresion3deco@gmail.com"; 
+
+    // Email subject
+    $subject = "Nuevo mensaje de $nombre desde el formulario de 3Deco";
+
+    // Email body
+    $body = "Nombre: $nombre\nEmail: $email\n\nMensaje:\n$mensaje";
+
+    // Email headers
+    $headers = "From: $email";
+
+    // Send email
+    if (mail($to, $subject, $body, $headers)) {
+        echo "Mensaje enviado con éxito.";
+    } else {
+        echo "Error al enviar el mensaje.";
+    }
+
 } else {
     // Mostrar un mensaje de error o redirigir si el usuario no pasó la validación
-    // ...
+    echo "Error en la verificación del captcha.";
+    //SUS 
+
 }
 ?>
